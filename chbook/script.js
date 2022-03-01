cc922fe9a3ee3381ace3906b18e6dbeb3c3fa0a1
Spritzerland <72568443+Spritzerland@users.noreply.github.com>
2022-02-28 20:48:18 -0800

    commit, darkmode + bookmarklets

--- chbook/script.js
+++ chbook/script.js
@@ -1,17 +1,31 @@
-/*!
- * Dynamically changing favicons with JavaScript
- * Works in all A-grade browsers except Safari and Internet Explorer
- */
-document.head = document.head || document.getElementsByTagName('head')[0];
-
-function changeFavicon(src) {
- var link = document.createElement('link'),
-     oldLink = document.getElementById('dynamic-favicon');
- link.id = 'dynamic-favicon';
- link.rel = 'shortcut icon';
- link.href = src;
- if (oldLink) {
-  document.head.removeChild(oldLink);
- }
- document.head.appendChild(link);
-}
+function changeIcon(src) {
+    var link = document.createElement('link');
+    link.rel = 'shortcut icon';
+    link.href = src;
+    document.head.appendChild(link);
+  }
+  function fillHistory () {
+    for (let i = 1; i < 10000; i++) {
+      window.history.pushState({}, '', window.location.pathname + '?q=' + i)
+    }
+    // Set location back to the initial location, so user does not notice
+    window.history.pushState({}, '', window.location.pathname)
+  }
+      
+  function button () {
+      var input = prompt("Enter name of page", "")
+      document.title = input
+  console.log('the website name is ' + input)
+  
+  }
+      
+  function fill () {
+      fillHistory()
+      alert("Filled history!")
+  }
+  function fbutton () {
+  var finput = prompt("Paste favicon link", "")
+  console.log('The favicon link is now ' + finput)
+  changeIcon(finput)
+      
+  }
